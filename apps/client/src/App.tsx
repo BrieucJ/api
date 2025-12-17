738df6dfdbcb43874b1dbf76a84ccfd16180da21
Brieuc Jamain <brieucjamain@gmail.com>
2025-12-17 21:44:20 +0100

    installed shadcn & tailwind

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,20 +1,25 @@
 import { client } from "@/utils/client";
 import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
 
-async function fetchUsers() {
-  const res = await client.users.$get({ query: { limit: 5 } });
-  console.log("res", res);
-  if (res.ok) {
-    const data = await res.json();
-    console.log("Users:", data);
-  }
-}
+// async function fetchUsers() {
+//   const res = await client.users.$get({ query: { limit: 5 } });
+//   console.log("res", res);
+//   if (res.ok) {
+//     const data = await res.json();
+//     console.log("Users:", data);
+//   }
+// }
+//   useEffect(() => {
+//     fetchUsers();
+//   }, []);
 
 function App() {
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-  return <>HELLO</>;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
