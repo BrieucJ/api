e6068dbc6ef87bad56a66aeb6602df1e20d16170
Brieuc Jamain <brieucjamain@gmail.com>
2026-01-02 01:47:31 +0100

    ok

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "drizzle-kit";
+import env from "./src/env";
 
 export default defineConfig({
   out: "./src/migrations",
   schema: "./src/db/models",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: env.DATABASE_URL,
   },
 });
