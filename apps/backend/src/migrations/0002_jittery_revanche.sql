bf366a155ac290ef58eae2164ee91d77dfca3e81
Brieuc Jamain <brieucjamain@gmail.com>
2026-01-02 21:56:48 +0100

    improved client

--- apps/backend/src/migrations/0002_jittery_revanche.sql
+++ apps/backend/src/migrations/0002_jittery_revanche.sql
@@ -1,1 +1,1 @@
-CREATE INDEX "worker_stats_last_heartbeat_idx" ON "worker_stats" USING btree ("last_heartbeat");
+CREATE INDEX IF NOT EXISTS "worker_stats_last_heartbeat_idx" ON "worker_stats" USING btree ("last_heartbeat");
