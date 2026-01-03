fa3e1d7043434fc2351b4a8c62fbf6ec2608beaa
Brieuc Jamain <brieucjamain@gmail.com>
2026-01-03 15:15:33 +0100

    fix envs

--- apps/backend/src/db/db.ts
+++ apps/backend/src/db/db.ts
@@ -3,7 +3,7 @@ import postgres from "postgres";
 import env from "@/env";
 import { dbLogger } from "@/utils/logger";
 
-const client = postgres(env.DATABASE_URL!, {
+const client = postgres(env.DATABASE_URL, {
   max: 3, // Allow 3 connections for parallel queries (Lambda can handle this)
   idle_timeout: 60000, // 60 seconds
   connect_timeout: 10, // 10 second connection timeout
